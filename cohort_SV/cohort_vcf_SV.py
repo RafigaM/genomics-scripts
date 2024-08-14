d6711ef444faae2eb70cc2cf22572e1be65a52d5
Rafiga Masmaliyeva <47282203+RafigaM@users.noreply.github.com>
2024-08-14 11:27:06 +0200

    Update cohort_vcf_SV.py

--- cohort_SV/cohort_vcf_SV.py
+++ cohort_SV/cohort_vcf_SV.py
@@ -22,13 +22,11 @@ def convert_array_to_scalar(arr):
 
 
 def get_samples(input):
-    #print(input)
     samples = []
     for subdir, dirs, files in os.walk(input):
         for file in files:
             filepath = subdir + os.sep + file
             if filepath.endswith(".vcf"):
-                #print(filepath)
                 samples.append(filepath)
 
     return samples
@@ -36,6 +34,7 @@ def get_samples(input):
 
 def generate_cohort(input):
     samples = get_samples(input)
+    #if '.DS_Store' in samples: samples.remove('.DS_Store')
     my_dict = defaultdict(lambda: {"CHROM": [], "POS": [], "ID": [], "REF": [], "ALT": [], "QUAL": [], "FILTER": [], "INFO": [], "FORMAT": [], "sample": [], "num": 0})
     for vcf in samples:
         if Path(vcf).is_file():
@@ -45,7 +44,8 @@ def generate_cohort(input):
                 FT = callset['variants/FILTER_PASS'][record]
                 if FT:
                     SV_type = str(callset['variants/SVTYPE'][record])
-                    sv_len = abs(callset['variants/SVLEN'][record])
+                    sv_len = callset['variants/SVLEN'][record]
+                    #if SV_type == "DEL":
                     if SV_type == "DEL":
                         chrom = callset['variants/CHROM'][record]
                         pos = callset['variants/POS'][record]
@@ -64,21 +64,18 @@ def generate_cohort(input):
                         my_dict[variant_key]['QUAL'] = int(qual)
                         my_dict[variant_key]['FILTER'] = 'PASS'
                         end = callset['variants/END'][record]
-                        svtype = callset['variants/SVLEN'][record]
-                        my_dict[variant_key]['INFO'] = "END=" + str(end) + ";SVTYPE=" + str(svtype) + ";SVLEN=" + str(svlen) # + ";"
+                        svtype = callset['variants/SVTYPE'][record]
+                        my_dict[variant_key]['INFO'] = "END=" + str(end) + ";SVTYPE=" + str(svtype) + ";SVLEN=" + str(sv_len) # + ";"
                         my_dict[variant_key]['FORMAT'] = 'GT:GQ:FT'
                         gt = convert_array_to_scalar(callset['calldata/GT'][record][0])
                         gq = callset['calldata/GQ'][record][0]
                         my_dict[variant_key]['sample'] = gt + ":" + str(gq) + ":" + "PASS"
                         my_dict[variant_key]['num'] += 1
 
-    normal_dict = {key: value for key, value in my_dict.items()}
-    inner_dict = next(iter(normal_dict.values()))
-
-    return inner_dict
+    return my_dict
 
 
-def write2vcf(output_file, inner_dict):
+def write2vcf(output_file, converted_dict):
     # VCF file header
     vcf_header = [
         "##fileformat=VCFv4.2",
@@ -90,29 +87,26 @@ def write2vcf(output_file, inner_dict):
         "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample"
     ]
 
-    # Extracting VCF row from the inner dictionary
-    vcf_row = [
-        inner_dict['CHROM'],
-        inner_dict['POS'],
-        inner_dict['ID'],
-        inner_dict['REF'],
-        inner_dict['ALT'],
-        inner_dict['QUAL'],
-        inner_dict['FILTER'],
-        inner_dict['INFO'],
-        inner_dict['FORMAT'],
-        inner_dict['sample'],
-        f"num={inner_dict['num']}",
-    ]
-
-    # Writing to the VCF file
     with open(output_file, 'w') as file:
         # Write header
         for line in vcf_header:
             file.write(line + "\n")
-
-        # Write the data row
-        file.write('\t'.join(map(str, vcf_row)) + "\n")
+        for inner_dict in converted_dict:
+            vcf_row = [
+                inner_dict['CHROM'],
+                inner_dict['POS'],
+                inner_dict['ID'],
+                inner_dict['REF'],
+                inner_dict['ALT'],
+                inner_dict['QUAL'],
+                inner_dict['FILTER'],
+                inner_dict['INFO'],
+                inner_dict['FORMAT'],
+                inner_dict['sample'],
+                f"num={inner_dict['num']}",
+            ]
+            # Join the elements with a tab and write to the file
+            file.write('\t'.join(map(str, vcf_row)) + "\n")
 
 
 def write2bed(output_file, inner_dict):
@@ -122,13 +116,17 @@ def write2bed(output_file, inner_dict):
 
 
 def main():
-    inner_dict = generate_cohort(folder)
+    folder = "/path/to/the/folder"
+    output_vcf = "cohort.vcf"
+    output_bed = "cohort.bed"
+    
+    my_dict = generate_cohort(folder)
+    normal_dict = {key: value for key, value in my_dict.items()}
+    df = pd.DataFrame.from_dict(normal_dict, orient='index')
+    converted_dict = df.to_dict(orient='records')
 
-    output_vcf = "output.vcf"
-    output_bed = "output.bed"
-
-    write2vcf(output_vcf, inner_dict)
-    write2bed(output_bed, inner_dict)
+    write2vcf(output_vcf, converted_dict)
+    write2bed(output_bed, converted_dict)
 
 
 
